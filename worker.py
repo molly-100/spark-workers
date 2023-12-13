28906f9a0659f2bf3db4d3c564bbd4cd3e0ee29e
molly-100 <125601191+molly-100@users.noreply.github.com>
2023-12-13 15:37:36 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -29,6 +29,7 @@ def add():
     return "Use post to add" # replace with form template
   else:
     token=get_api_key()
+      token= token.strip("\n")
     ret = addWorker(token,request.form['num'])
     return ret
 
